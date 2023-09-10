7b60e8e41527532706a0274d33171810b3aca366
Khoa Nguyen <nguyendangkhoa.3118410198@gmail.com>
2023-09-10 11:41:42 +0700

    Change Layout in /AdminLogin & add icon into /Header

--- src/components/AdminLogin/index.js
+++ src/components/AdminLogin/index.js
@@ -2,38 +2,49 @@ import React, { useState } from "react";
 import "./AdminLogin.css";
 
 const Login = () => {
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-   const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(`Email: ${email}, Password: ${password}`);
-   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Email: ${email}, Password: ${password}`);
+  };
 
-   return (
-      <div className="login-container">
-         <h1 className="name-form">Login</h1>
-         <form onSubmit={handleSubmit}>
-            <label className="label-name">Email:</label>
-            <input
-               type="text"
-               value={email}
-               onChange={(e) => setEmail(e.target.value)}
-               required
-            />
-            <label className="label-name">Password:</label>
-            <input
-               type="password"
-               value={password}
-               onChange={(e) => setPassword(e.target.value)}
-               required
-            />
-            <button className="button-login" type="submit">
-               Login
-            </button>
-         </form>
+  return (
+    <div className="login-wrapper">
+      <div className="admin-form-container">
+        <h1 className="name-form">Login</h1>
+        <form className="admin-form" onSubmit={handleSubmit}>
+          <label className="label-name" htmlFor="emailInput">
+            Email
+          </label>
+          <input
+            id="emailInput"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder=" abc@gmail.com"
+            required
+          />
+          <label className="label-name" htmlFor="passwordInput">
+            Password
+          </label>
+          <input
+            id="passwordInput"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder=" xxxxxxxx"
+            required
+          />
+          <button className="button-login" type="submit">
+            Login
+          </button>
+        </form>
+        <p className="forgot-password">Forgot your password ?</p>
       </div>
-   );
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,86 +2,94 @@ import React, { useState, useRef, useEffect } from "react";
 
 import "./Header.css";
 import {
-   UilUser,
-   UilInfoCircle,
-   UilQuestionCircle,
-   UilSignout,
+  UilUser,
+  UilInfoCircle,
+  UilQuestionCircle,
+  UilSignout,
 } from "@iconscout/react-unicons";
+import { FaBell, FaCalendarCheck, FaQuestionCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Header = ({ nameContent }) => {
-   const [isMenuOpen, setIsMenuOpen] = useState(false);
-   const menuRef = useRef(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
 
-   const toggleMenu = () => {
-      setIsMenuOpen(!isMenuOpen);
-   };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-   const handleOutsideClick = (event) => {
-      if (!menuRef.current.contains(event.target)) {
-         setIsMenuOpen(false);
-      }
-   };
+  const handleOutsideClick = (event) => {
+    if (!menuRef.current.contains(event.target)) {
+      setIsMenuOpen(false);
+    }
+  };
 
-   useEffect(() => {
-      document.addEventListener("mousedown", handleOutsideClick);
-      return () => {
-         document.removeEventListener("mousedown", handleOutsideClick);
-      };
-   }, [menuRef]);
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [menuRef]);
 
-   return (
-      <header className="Header">
-         <div>
-            <h1>{nameContent}</h1>
-         </div>
+  return (
+    <header className="Header">
+      <div>
+        <h1>{nameContent}</h1>
+      </div>
 
-         <div ref={menuRef}>
-            <nav>
-               <div className="user-wrapper">
-                  <span>Xin chao : Admin </span>
-                  <div className="user-header" onClick={toggleMenu}>
-                     <UilUser className="user-header__icon" />
-                  </div>
-               </div>
-               <ul className="menu-user">
-                  {isMenuOpen && (
-                     <ul className="dropdown-menu">
-                        <li>
-                           <Link to="/">
-                              <div className="menu-item">
-                                 <UilInfoCircle className="icon-menu-item" />
-                                 <span className="menu-item-content">Info</span>
-                              </div>
-                           </Link>
-                        </li>
-                        <li>
-                           <Link to="/orders">
-                              <div className="menu-item">
-                                 <UilQuestionCircle className="icon-menu-item" />
-                                 <span className="menu-item-content">
-                                    Feedback
-                                 </span>
-                              </div>
-                           </Link>
-                        </li>
-                        <li>
-                           <Link to="/login">
-                              <div className="menu-item">
-                                 <UilSignout className="icon-menu-item" />
-                                 <span className="menu-item-content">
-                                    Logout
-                                 </span>
-                              </div>
-                           </Link>
-                        </li>
-                     </ul>
-                  )}
-               </ul>
-            </nav>
-         </div>
-      </header>
-   );
+      <div ref={menuRef}>
+        <nav>
+          <div className="user-wrapper">
+            <div className="icon-menu-item">
+              <FaBell />
+            </div>
+            <div className="icon-menu-item">
+              <FaCalendarCheck />
+            </div>
+            <div className="icon-menu-item">
+              <FaQuestionCircle />
+            </div>
+
+            <span>Xin chao : Admin </span>
+
+            <div className="user-header" onClick={toggleMenu}>
+              <UilUser className="user-header__icon" />
+            </div>
+          </div>
+          <ul className="menu-user">
+            {isMenuOpen && (
+              <ul className="dropdown-menu">
+                <li>
+                  <Link to="/">
+                    <div className="menu-item">
+                      <UilInfoCircle className="icon-menu-item-dropdown" />
+                      <span className="menu-item-content">Info</span>
+                    </div>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/orders">
+                    <div className="menu-item">
+                      <UilQuestionCircle className="icon-menu-item-dropdown" />
+                      <span className="menu-item-content">Feedback</span>
+                    </div>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/login">
+                    <div className="menu-item">
+                      <UilSignout className="icon-menu-item-dropdown" />
+                      <span className="menu-item-content">Logout</span>
+                    </div>
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
