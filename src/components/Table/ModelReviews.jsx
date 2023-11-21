72af9047ee49531afe84f0a7b6125570100d14c2
Khoa Nguyen <nguyendangkhoa.3118410198@gmail.com>
2023-11-21 13:37:53 +0700

    css animation for totolist and add useState loading for ModalRivews

--- src/components/Table/ModelReviews.jsx
+++ src/components/Table/ModelReviews.jsx
@@ -1,12 +1,22 @@
+import { useState, useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import './review.css';
 
 function ModalReviews(props) {
+   const [loading, setLoading] = useState(true);
    const dataReview = props.data;
 
    const capitalizeFirstLetter = (str) => {
       return str.charAt(0).toUpperCase() + str.slice(1);
    };
+
+   useEffect(() => {
+      const timeoutId = setTimeout(() => {
+         setLoading(false);
+      }, 3000);
+
+      return () => clearTimeout(timeoutId);
+   }, []);
 
    return (
       <Modal
@@ -21,23 +31,29 @@ function ModalReviews(props) {
             </Modal.Title>
          </Modal.Header>
          <Modal.Body>
-            {dataReview && dataReview.length > 0 ? (
-               dataReview.map((review, index) => (
-                  <div className='reviews-container' key={index}>
-                     {Object.entries(review).map(([key, value]) =>
-                        key !== 'id' && key !== 'customerId' ? (
-                           <p key={key}>
-                              <strong>{`${capitalizeFirstLetter(
-                                 key
-                              )}:`}</strong>{' '}
-                              {value}
-                           </p>
-                        ) : null
-                     )}
-                  </div>
-               ))
+            {loading ? (
+               <p>Loading...</p>
             ) : (
-               <p>No reviews found.</p>
+               <>
+                  {dataReview && dataReview.length > 0 ? (
+                     dataReview.map((review, index) => (
+                        <div className='reviews-container' key={index}>
+                           {Object.entries(review).map(([key, value]) =>
+                              key !== 'id' && key !== 'customerId' ? (
+                                 <p key={key}>
+                                    <strong>{`${capitalizeFirstLetter(
+                                       key
+                                    )}: `}</strong>
+                                    {value}
+                                 </p>
+                              ) : null
+                           )}
+                        </div>
+                     ))
+                  ) : (
+                     <p>No reviews found.</p>
+                  )}
+               </>
             )}
          </Modal.Body>
       </Modal>
