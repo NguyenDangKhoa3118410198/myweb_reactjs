38c88e1b27ca26d343eb19742372079d5e4c826e
Khoa Nguyen <nguyendangkhoa.3118410198@gmail.com>
2023-11-30 22:36:30 +0700

    cre: handle get all orders in backend

--- backend/src/controllers/OrderController.js
+++ backend/src/controllers/OrderController.js
@@ -1,5 +1,32 @@
-const getOrders = (req, res) => {
-   return res.send('Orders: sdsd');
+const axios = require('axios');
+const orders = [];
+
+const getOrders = async (req, res) => {
+   console.log('get orders');
+
+   try {
+      const response = await axios.get('https://dummyjson.com/carts');
+      const data = response.data.carts;
+      const updatedData = data
+         .map((order) => {
+            return order.products.map((product) => {
+               return {
+                  id: product.id,
+                  title: product.title,
+                  price: product.price,
+                  amount: product.quantity,
+                  total: product.total,
+               };
+            });
+         })
+         .flat();
+
+      orders.push(updatedData);
+      res.json(orders);
+   } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+   }
 };
 
 module.exports = { getOrders };
--- backend/src/routes/order.js
+++ backend/src/routes/order.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const orderController = require('../controllers/OrderController');
 
-router.get('/getAllOrders', orderController.getOrders);
+router.get('/', orderController.getOrders);
+// router.post('/add', orderController.addUser);
+// router.delete('/:id', orderController.deleteUser);
+// router.post('/:id/edit', orderController.editUser);
 
 module.exports = router;
