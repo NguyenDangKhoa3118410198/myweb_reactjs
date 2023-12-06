b957f62ec0d3a54219ecf05384a03dde48c063de
Khoa Nguyen <nguyendangkhoa.3118410198@gmail.com>
2023-12-06 15:12:33 +0700

    delete table 2 in page product

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { pageProducts1, pageProducts2 } from '../../Data/fetchData';
-import { columnsProduct1, columnsProduct2 } from '../../Data/columns';
+import { pageProducts1 } from '../../Data/fetchData';
+import { columnsProduct1 } from '../../Data/columns';
 import Table from '../../components/Table/Table';
 import {
    searchBox,
@@ -14,7 +14,6 @@ import './product.css';
 function Products() {
    const darkMode = useSelector((state) => state.darkMode);
 
-   const [records, setRecords] = useState([]);
    const [products, setProducts] = useState([]);
    const [searchTerm, setSearchTerm] = useState('');
 
@@ -23,10 +22,6 @@ function Products() {
 
    useEffect(() => {
       pageProducts1(setProducts);
-   }, []);
-
-   useEffect(() => {
-      pageProducts2(setRecords);
    }, []);
 
    function filterData(records) {
@@ -54,26 +49,18 @@ function Products() {
       <main className={`product-wrapper  ${darkMode ? 'darkmode' : ''}`}>
          <div id='top' style={{ opacity: '0' }}></div>
 
-         <div className='product-table-item1'>
-            <Table
-               title={'List products'}
-               columns={columns}
-               data={filterData(products)}
-               searchBox={searchBox(searchTerm, setSearchTerm)}
-               tableActions={{
-                  setModalReview,
-                  isListReviews,
-                  isModalReview,
-               }}
-            />
-         </div>
-         <div className='product-table-item2'>
-            <Table
-               columns={columnsProduct2}
-               data={filterData(records)}
-               searchBox={searchBox(searchTerm, setSearchTerm)}
-            />
-         </div>
+         <Table
+            title={'List products'}
+            columns={columns}
+            data={filterData(products)}
+            searchBox={searchBox(searchTerm, setSearchTerm)}
+            tableActions={{
+               setModalReview,
+               isListReviews,
+               isModalReview,
+            }}
+         />
+
          <OnTopButton />
       </main>
    );
