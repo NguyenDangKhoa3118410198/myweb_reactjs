0b1544f97b4cdb8da1e8f785978ab97f4c470b8b
Khoa Nguyen <nguyendangkhoa.3118410198@gmail.com>
2023-12-15 22:22:07 +0700

    install nodemailer and cre route to sendEmail

---
+++ backend/src/controllers/EmailController.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (req, res) => {
+   const { email } = req.body;
+
+   const emailUser = process.env.EMAIL_USER;
+   const emailPassword = process.env.EMAIL_PASSWORD;
+
+   const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+         user: emailUser,
+         pass: emailPassword,
+      },
+   });
+
+   const mailOptions = {
+      from: emailUser,
+      to: email,
+      subject: 'Test Email',
+      text: 'Hello, this is a test email!',
+   };
+
+   try {
+      const info = await transporter.sendMail(mailOptions);
+      console.log('Email sent: ' + info.response);
+      res.json({ success: true, message: 'Email sent successfully' });
+   } catch (error) {
+      console.error('Error sending email:', error);
+      res.json({ success: false, message: 'Failed to send email' });
+   }
+};
+
+module.exports = { sendEmail };
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,8 @@ const orderRouter = require('./order');
 const productRouter = require('./product');
 const customerRouter = require('./customer');
 const todoRouter = require('./todo');
+
+const emailRouter = require('./email');
 const loginRouter = require('./login');
 const logoutRouter = require('./logout');
 const registerRouter = require('./register');
@@ -16,6 +18,8 @@ function route(app) {
    app.use('/api/products', authenticateToken, productRouter);
    app.use('/api/customers', authenticateToken, customerRouter);
    app.use('/api/todo', authenticateToken, todoRouter);
+
+   app.use('/auth/email', emailRouter);
    app.use('/auth/login', loginRouter);
    app.use('/auth/logout', logoutRouter);
    app.use('/auth/register', registerRouter);
