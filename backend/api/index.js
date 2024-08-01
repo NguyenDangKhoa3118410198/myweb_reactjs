3dce16d559f2bef5c868caf733f34218bf8405e0
Khoa Nguyen <nguyendangkhoa.3118410198@gmail.com>
2024-08-01 20:48:47 +0700

    .

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const db = require('./src/models/db');
+const db = require('../src/models/db');
 require('dotenv').config();
 const port = process.env.PORT || 4000;
 
-const route = require('./src/routes');
+const route = require('../src/routes');
 
 const corsOptions = {
    origin: 'http://localhost:3000', // Chỉ chấp nhận yêu cầu từ nguồn này
