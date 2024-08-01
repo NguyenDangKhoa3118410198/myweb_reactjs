ce9f9285f2ed6ab43635fa5639cf40e9f0e119ae
Khoa Nguyen <nguyendangkhoa.3118410198@gmail.com>
2024-08-01 21:04:33 +0700

    /

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -37,6 +37,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Express on Vercel'));
+app.get('/test', (req, res) => res.send('DC?M'));
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
 
