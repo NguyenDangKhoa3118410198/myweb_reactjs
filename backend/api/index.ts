64ac9d12b3e226828187766cd4d82b6b84d676e3
Khoa Nguyen <nguyendangkhoa.3118410198@gmail.com>
2024-08-01 21:09:37 +0700

    .

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -34,4 +34,8 @@ route(app);
 //   console.log(`Server is running on port ${port}`);
 // });
 
+app.get('/', (req, res) => res.send('Express on Vercel'));
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
+
 module.exports = app;
